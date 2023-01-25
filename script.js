4b105a77edb1d3ef7d871661ca72b04709d1981c
ripalkumar <patelripalacc@gmail.com>
2023-01-25 15:17:00 -0500

    create repo

---
+++ script.js
@@ -0,0 +1,51 @@
+// const storyPieces = [
+//     "Lord of the Rings",
+//     "J. K. Rowling",
+//     "Luke Skywalker",
+//     "Thanos",
+//     "Robin",
+//     "Edward",
+//     "Jacob",
+//   ];
+  
+//   //1.
+//   //Create the following 4 variables: "title", "author", "hero", and "villain"
+//   //and assign them to the first 4 elements in storyPieces
+//   let title = storyPieces[0];
+//   let author = storyPieces[1];
+//   let hero = storyPieces[2];
+//   let villain = storyPieces[3];
+
+//   console.log(title,author,hero,villain);
+
+//   //2.
+//   //Create two variables "book" and "writer"
+//   //and assign them to the first 2 elements in storyPieces
+//   //Create a third variable "theRest"
+//   //and assign it to store the remaining 5 elements' values in storyPieces
+
+//   let book = storyPieces[0];
+//   let writer = storyPieces[1];
+//   let theRest = [storyPieces[2],storyPieces[3],storyPieces[4],storyPieces[5],storyPieces[6]];
+  
+// //   const [book, writer, ...theRest] = storyPieces;
+//   console.log(book, writer, theRest);
+  
+  const storyInfo = {
+    title: "Lord of the Rings",
+    author: "J. K. Rowling",
+    protagonists: "Luke Skywalker",
+    antagonist: "Thanos",
+    sidekick: "Robin",
+    loveInterest1: "Edward",
+    loveInterest2: "Jacob",
+  };
+  
+//   //3.
+//   //Create the following 4 variables: "title", "author", "protagonist", and "antagonist"
+  //and assign them to the first 4 property values in storyInfo
+  let title =  storyInfo.title;
+  let author = storyInfo.author;
+  let protagonists = storyInfo.protagonists;
+  let antagonist = storyInfo.antagonist;
+
